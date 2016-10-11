1e56d16b4aa1dd04aaaa2d0c14996abc39eb3c8e
Yamakaky <yamakaky@yamaworld.fr>
2016-10-11 16:38:46 -0400

    Much changes

--- src/load.rs
+++ src/load.rs
@@ -14,6 +14,25 @@ error_chain! {
     }
 }
 
+struct Request<'a, T: 'a> {
+    buf: &'a mut [i8; libmnl_sys::socket::BUFFER_SIZE],
+    len: usize,
+    callback: callback::cb_t,
+    data: &'a mut T,
+}
+
+impl<'a, T> socket::Request for Request<'a, T> {
+    fn data(&mut self) -> *mut c_void {
+        self.buf.as_mut_ptr() as *mut c_void
+    }
+    fn len(&self) -> usize {
+        self.len
+    }
+    fn callback(&mut self) -> (callback::cb_t, *mut c_void) {
+        (self.callback, self.data as *mut T as *mut c_void)
+    }
+}
+
 impl Table {
     pub fn load_all() -> Result<Vec<Table>> {
 
@@ -22,28 +41,10 @@ impl Table {
                                       -> i32 {
             let tables = &mut *(tables as *mut Vec<Table>);
             tables.push(Table::decode(header).unwrap());
-            libmnl_sys::callback::CallbackResult::MNL_CB_OK as i32
+            callback::CallbackResult::MNL_CB_OK as i32
         }
 
         unsafe {
-            struct Request<'a> {
-                buf: [i8; libmnl_sys::socket::BUFFER_SIZE],
-                len: usize,
-                tables: &'a mut Vec<Table>,
-            }
-
-            impl<'a> socket::Request for Request<'a> {
-                fn data(&mut self) -> *mut c_void {
-                    self.buf.as_mut_ptr() as *mut c_void
-                }
-                fn len(&self) -> usize {
-                    self.len
-                }
-                fn callback(&mut self) -> (callback::cb_t, *mut c_void) {
-                    (Some(table_cb), self.tables as *mut Vec<Table> as *mut c_void)
-                }
-            }
-
             let seq = 0;
             let mut buf = [0; libmnl_sys::socket::BUFFER_SIZE];
             let header =
@@ -57,9 +58,10 @@ impl Table {
             {
                 let mut socket = try!(socket::Socket::open());
                 let mut request = Request {
-                    buf: buf,
+                    buf: &mut buf,
                     len: (*header).nlmsg_len as usize,
-                    tables: &mut tables,
+                    callback: Some(table_cb),
+                    data: &mut tables,
                 };
                 try!(socket.exec_request(&mut request));
             }
@@ -76,28 +78,10 @@ impl Chain {
                                       -> i32 {
             let chain = &mut *(chain as *mut Chain);
             *chain = Chain::decode(header).unwrap();
-            libmnl_sys::callback::CallbackResult::MNL_CB_STOP as i32
+            callback::CallbackResult::MNL_CB_STOP as i32
         }
 
         unsafe {
-            struct Request<'a> {
-                buf: [i8; libmnl_sys::socket::BUFFER_SIZE],
-                len: usize,
-                chain: &'a mut Chain,
-            }
-
-            impl<'a> socket::Request for Request<'a> {
-                fn data(&mut self) -> *mut c_void {
-                    self.buf.as_mut_ptr() as *mut c_void
-                }
-                fn len(&self) -> usize {
-                    self.len
-                }
-                fn callback(&mut self) -> (callback::cb_t, *mut c_void) {
-                    (Some(chain_cb), self.chain as *mut Chain as *mut c_void)
-                }
-            }
-
             let seq = 0;
             let mut buf = [0; libmnl_sys::socket::BUFFER_SIZE];
             let header =
@@ -125,13 +109,59 @@ impl Chain {
             {
                 let mut socket = try!(socket::Socket::open());
                 let mut request = Request {
-                    buf: buf,
+                    buf: &mut buf,
                     len: (*header).nlmsg_len as usize,
-                    chain: &mut chain,
+                    callback: Some(chain_cb),
+                    data: &mut chain,
                 };
                 try!(socket.exec_request(&mut request));
             }
             Ok(chain)
+        }
+    }
+
+    pub fn load_table(family: Family, table: &str) -> Result<Vec<Chain>> {
+
+        unsafe extern "C" fn chain_cb(header: *const libmnl_sys::nlmsghdr,
+                                      chain: *mut c_void)
+                                      -> i32 {
+            let chains = &mut *(chain as *mut Vec<Chain>);
+            chains.push(Chain::decode(header).unwrap());
+            callback::CallbackResult::MNL_CB_OK as i32
+        }
+
+        unsafe {
+            let seq = 0;
+            let mut buf = [0; libmnl_sys::socket::BUFFER_SIZE];
+            let header =
+                common::nlmsg_build_hdr(buf.as_mut_ptr(),
+                                        nf_tables::nf_tables_msg_types::NFT_MSG_GETCHAIN as u16,
+                                        family as u16,
+                                        libmnl_sys::socket::NLM_F_DUMP,
+                                        seq);
+            let table = CString::new(table).unwrap();
+
+            let chain = chain::alloc();
+            assert!(chain != ptr::null_mut());
+            chain::set(chain,
+                       chain::chain_attr::TABLE as u16,
+                       table.as_ptr() as *const c_void);
+            chain::nlmsg_build_payload(header, chain);
+            chain::free(chain);
+
+
+            let mut chains = vec![];
+            {
+                let mut socket = try!(socket::Socket::open());
+                let mut request = Request {
+                    buf: &mut buf,
+                    len: (*header).nlmsg_len as usize,
+                    callback: Some(chain_cb),
+                    data: &mut chains,
+                };
+                try!(socket.exec_request(&mut request));
+            }
+            Ok(chains)
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@ extern crate rftables;
 use rftables::*;
 
 fn main() {
-    let chain = Chain::load(Family::Inet, "filter", "pote").unwrap();
+    let chain = Chain::load(Family::Inet, "filter", "input").unwrap();
     println!("{:?}", chain);
+    println!("{:?}", Chain::load_table(Family::Inet, "filter"));
     println!("{:?}", Table::load_all());
 }
